29c229344a0c0f3c66b7d47cef902391a2549cdc
hashirhk <mhashir1004@gmail.com>
2025-01-22 14:11:58 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,55 @@
-import {useState} from 'react'
-
+import { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
 
-const [count , setCount] = useState(0);
-const increase = () =>{
-  setCount(count+1)
+  const increase = () => {
+    setCount(count + 1);
+  };
 
-}
-const decrease = () =>{
-  setCount(count-1)
-}
+  const decrease = () => {
+    setCount(count - 1);
+  };
 
-const refresh = () =>{
-  setCount(count*0)
-
-}
+  const refresh = () => {
+    setCount(0);
+  };
 
   return (
-   
+    <div
+      className="h-[100vh] flex items-center justify-center flex-col space-y-7 text-white bg-black"
+    >
+      <div>
+      <h1 className="text-4xl font-extrabold tracking-wide mb-4 bg-gradient-to-tr from-orange-500 to-purple-800 bg-clip-text text-transparent">
+  Hashiiiii Counter!!!
+</h1>
 
-<div className='bg-gray-500 text-black h-[100vh] flex items-center justify-center flex-col space-y-7'>
-<div>
-<h1 className='text-2xl font-bold '  >MY Counter</h1>
-</div>
+      </div>
 
-<span>{count}</span>
+      <span className="text-5xl font-semibold mb-4">{count}</span>
 
-
-    <div className='flex space-x-5 '>
-    <button className='bg-green-600 rounded-md p-3 border border-black  hover:bg-green-200' onClick={increase} >Add</button>
-    <button  className='bg-red-600 rounded-md p-3 border border-black  hover:bg-green-200' onClick={decrease} >Subtract</button>
-    <button className='bg-blue-600 rounded-md p-3 border border-black  hover:bg-green-200' onClick={refresh} >Reset</button>
-
+      <div className="flex space-x-5">
+        <button
+          className="bg-green-500 rounded-lg px-6 py-3 shadow-lg transform transition duration-300 hover:bg-green-400 hover:-translate-y-1"
+          onClick={increase}
+        >
+          Add
+        </button>
+        <button
+          className="bg-red-500 rounded-lg px-6 py-3 shadow-lg transform transition duration-300 hover:bg-red-400 hover:-translate-y-1"
+          onClick={decrease}
+        >
+          Subtract
+        </button>
+        <button
+          className="bg-blue-500 rounded-lg px-6 py-3 shadow-lg transform transition duration-300 hover:bg-blue-400 hover:-translate-y-1"
+          onClick={refresh}
+        >
+          Reset
+        </button>
+      </div>
     </div>
+  );
+};
 
-
-    </div>
-    
-  )
-}
-
-export default App
+export default App;
